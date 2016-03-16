be7b5e01ecfa6b36b16e4b1d262db0f3b580bad5
ETEJayCheng <jay.cheng@etechpro.com.tw>
2016-03-16 14:08:01 +0800

    update

---
+++ app/src/main/java/com/sks/iotnoti/FragmentHolderActivity.java
@@ -0,0 +1,15 @@
+
+package com.sks.iotnoti;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class FragmentHolderActivity extends  FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_holder);
+    }
+}
